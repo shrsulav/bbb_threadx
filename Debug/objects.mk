e0f888f376d41ffe1e43a41afd4d04ad0f7f841e
Sulav Lal Shrestha <slshrest@uwaterloo.ca>
2023-04-26 11:58:49 -0400

    cleanup

--- Debug/objects.mk
+++
@@ -1,8 +0,0 @@
-################################################################################
-# Automatically-generated file. Do not edit!
-################################################################################
-
-USER_OBJS :=
-
-LIBS := -llibc.a -l"C:/Users/sulavs/Downloads/ti_starterware/binary/armv7a/cgt_ccs/am335x/drivers/Debug/drivers.lib" -l"C:/Users/sulavs/Downloads/ti_starterware/binary/armv7a/cgt_ccs/am335x/system_config/Debug/system.lib" -l"C:/Users/sulavs/Downloads/ti_starterware/binary/armv7a/cgt_ccs/am335x/beaglebone/platform/Debug/platform.lib" -l"C:/Users/sulavs/Downloads/ti_starterware/binary/armv7a/cgt_ccs/utils/Debug/utils.lib"
-
